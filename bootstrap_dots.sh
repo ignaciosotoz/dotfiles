c3d247e6d26820cbdd4554f5435dd1b97dee78d1
ignacio.soto.z <ignacio.soto.z@gmail.com>
2018-08-11 21:38:21 -0400

    Fix home reference

--- bootstrap_dots.sh
+++ bootstrap_dots.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ln -s gitconfig $HOME/.gitconfig
-ln -s vimrc $HOME/.vimrc
-ln -s zshrc $HOME/.zshrc
-ln -s tmux.conf $HOME/.tmux.conf
+ln -s gitconfig ${HOME}/.gitconfig
+ln -s vimrc ${HOME}/.vimrc
+ln -s zshrc ${HOME}/.zshrc
+ln -s tmux.conf ${HOME}/.tmux.conf
